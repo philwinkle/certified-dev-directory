623a498a6997b2ef1fd891522fdff8e920c797db
Phillip Jackson <philwinkle@gmail.com>
2013-12-13 10:56:07 -0500

    Crawler correctly assesses pagination and adds all devs to an output.csv file (untracked)

--- run.php
+++ run.php
@@ -1,7 +1,9 @@
 <?php
+// ini_set('xdebug.var_display_max_depth', '-1');
+// ini_set('xdebug.var_display_max_children', '-1');
 
-require_once('goutte.phar');
 require 'vendor/autoload.php';
+require 'goutte.phar';
 
 use Goutte\Client;
 
@@ -9,10 +11,12 @@ class Crawl {
     
     protected $_client;
     const MAGENTO_DEV_DIR_URL = 'http://www.magentocommerce.com/certification/directory/?q=&in=&country_id=US&region_id=&region=&certificate_type=&p=';
+    const OUTPUT_FILENAME     = 'output.csv';
 
     public function __construct()
     {
-        $this->_client = new Client();
+        $this->_client  = new Client();
+        $this->dom      = new DOMDocument();
         $this->run();       
     }
 
@@ -21,21 +25,39 @@ class Crawl {
         $results = array();
         $preg    = array();
 
-        $crawler = $this->_client->request('GET', self::MAGENTO_DEV_DIR_URL);
-        $numPages = $crawler->filter('.pager .f-left');
+        $initCrawler = $this->_client->request('GET', self::MAGENTO_DEV_DIR_URL);
+        $numPages = $initCrawler->filter('a.last')->text();
 
-        preg_match('/of\s+?(.*?)\s+?results/i',$numPages->text(),$preg);
-
-        $limit = (int)$preg[1];
+        $limit = (int)$numPages;
 
         for($i=1;$i<=$limit;$i++){
             
             $crawler = $this->_client->request('GET', self::MAGENTO_DEV_DIR_URL . $i);
+            $numDevelopers = $crawler->filter('.results tr')->count();
+            // var_dump($numDevelopers);
 
-            $results[] = $crawler->filter('.results tr');
-            var_dump($results);
-            die();
+            for($j=1;$j<$numDevelopers;$j++){
+                $developer = $crawler->filter('.results tr')->eq($j);
+
+                $results[] = array(
+                    'name'          =>trim($developer->filter('.tb-col-00 b')->text()),
+                    'company'       =>trim($developer->filter('.tb-col-01')->text()),
+                    'location'      =>trim($developer->filter('.tb-col-02')->text()),
+                    'certification' =>trim($developer->filter('.tb-col-03')->text()),
+                    'date'          =>trim($developer->filter('.tb-col-04')->text())
+                );
+
+                echo "Processed record $j of page $i" . PHP_EOL;
+            }
+
         }
+
+        $output = array();
+        foreach($results as $line){
+            $output[] = join(',', $line);
+        }
+
+        file_put_contents(self::OUTPUT_FILENAME, join("\n",$output));
     }
 }
 
